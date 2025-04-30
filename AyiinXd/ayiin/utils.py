af43d6d7fe3f26b959fd6dff3a5126390cc2c3db
almairemran <almairemran@gmail.com>
2025-04-30 09:15:28 +0700

    @almairemran

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -77,7 +77,7 @@ async def autopilot():
     channel = get_peer_id(chat)
     if isinstance(chat.photo, ChatPhotoEmpty):
         photo = await download_file(
-            "https://telegra.ph/file/fd2b28637561809efff3d-43f95c514fe9149b94.jpg", "photoyins.jpg"
+            "https://telegra.ph/file/ea2e2a3309e43bb3c1fb3-7aa462ad4ee6efadc0.jpg", "photoyins.jpg"
         )
         ll = await bot.upload_file(photo)
         try:
@@ -135,7 +135,7 @@ async def autobot():
             sys.exit(1)
     filogo = random.choice(
           [
-              "https://telegra.ph/file/fd2b28637561809efff3d-43f95c514fe9149b94.jpg",
+              "https://telegra.ph/file/ea2e2a3309e43bb3c1fb3-7aa462ad4ee6efadc0.jpg",
               "AyiinXd/resources/logo.jpg",
           ]
     )
